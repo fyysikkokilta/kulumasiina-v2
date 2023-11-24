ca390f87612efa3ca8978e1881d800d7b359612c
Jaakko Sirén <jaakko.s@iki.fi>
2023-11-24 20:36:58 +0200

    Make db path configurable

--- backend/kulumasiina_backend/db.py
+++ backend/kulumasiina_backend/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+if 'DATABASE_URL' in os.environ:
+    raise Exception("DATABASE_URL not set. Please set it in .env")
+
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
