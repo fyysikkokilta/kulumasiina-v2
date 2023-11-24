34e21900814e8f5d644243a86419b8a91266453e
Jaakko Sirén <jaakko.s@iki.fi>
2023-11-24 21:23:30 +0200

    fix in to not in

--- backend/kulumasiina_backend/db.py
+++ backend/kulumasiina_backend/db.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-if 'DATABASE_URL' in os.environ:
+
+if 'DATABASE_URL' not in os.environ:
     raise Exception("DATABASE_URL not set. Please set it in .env")
 
 SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
