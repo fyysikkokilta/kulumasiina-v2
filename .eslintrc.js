9cc699774f7bdd5a247cd8dc5dd4ec7c7a483dc7
Jaakko Sirén <jaakko.s@iki.fi>
2023-11-26 14:23:25 +0200

    Enable format on save for vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }], //should add ".ts" if typescript project
+    "@typescript-eslint/no-unused-vars": [
+      1,
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-            // suppress errors for missing 'import React' in files
-            "react/react-in-jsx-scope": "off",
-            // allow jsx syntax in js files (for next.js project)
-            "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }], //should add ".ts" if typescript project
-            "@typescript-eslint/no-unused-vars": [1, { "varsIgnorePattern": "^_" , "argsIgnorePattern": "^_"}],
-    },
-    "ignorePatterns": ["frontend/dist/"]
-}
+  },
+  ignorePatterns: ["frontend/dist/"],
+};
