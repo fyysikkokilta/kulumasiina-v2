92604a6e5f3a47e135694f8f02b691311b398749
Aapo Laakkio <aapolaakkio@outlook.com>
2024-06-20 22:37:29 +0300

    Add a simple way to apply migrations

---
+++ migrations/001-bookkeeping-accounts/migrations.py
@@ -0,0 +1,9 @@
+from sqlite_migrate import Migrations
+
+migration = Migrations("Bookkeeping accounts")
+
+@migration()
+def add_bookkeeping_accounts(db):
+    # db is a sqlite-utils Database instance
+    db["item"].add_column("account", str)
+    db["mileage"].add_column("account", str)
