e2cb6e79725b75e772e18006c3177162581c1900
Aapo Laakkio <47868046+ApsiV11@users.noreply.github.com>
2025-06-20 15:59:23 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,9 +10,6 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
     useCache: true,
-    serverActions: {
-      allowedOrigins: ['localhost:8010', 'localhost:3000', env.BASE_URL.replace(/^https?:\/\//, '')]
-    },
     optimizePackageImports: ['antd', '@ant-design/icons', 'drizzle-orm']
   },
   transpilePackages: ['@t3-oss/env-nextjs', '@t3-oss/env-core'],
