c2d80b4f729aaca07c3d4351917367778d9d4ba2
Aapo Laakkio <aapolaakkio@outlook.com>
2025-08-20 19:01:40 +0300

    Don't use next lint

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
 const buildEslintCommand = (filenames: string[]) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 const lintStagedConfig = {
   '**/*.ts?(x)': () => 'tsc --noEmit',
