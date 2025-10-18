a3c2fb7a2b533e7dcc02ee1c02c725851e006bed
Aapo Laakkio <aapolaakkio@outlook.com>
2025-10-19 01:08:57 +0300

    chore: update next to last 15 canary version
    last version where ppr is not deprecated and hopefully the postponed state bug is fixed

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import { NextConfig } from 'next'
+import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
     useCache: true,
     optimizePackageImports: ['antd', '@ant-design/icons', 'drizzle-orm'],
     ppr: true
